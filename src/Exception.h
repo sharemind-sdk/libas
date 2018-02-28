3597a0df8fce07a2a5f8622213eabef6359b8ac5
Jaak Ristioja <jaak.ristioja@cyber.ee>
2018-02-28 10:04:31 +0200

    Added a base exception type.

---
+++ src/Exception.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) Cybernetica
+ *
+ * Research/Commercial License Usage
+ * Licensees holding a valid Research License or Commercial License
+ * for the Software may use this file according to the written
+ * agreement between you and Cybernetica.
+ *
+ * GNU General Public License Usage
+ * Alternatively, this file may be used under the terms of the GNU
+ * General Public License version 3.0 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.GPL included in the
+ * packaging of this file.  Please review the following information to
+ * ensure the GNU General Public License version 3.0 requirements will be
+ * met: http://www.gnu.org/copyleft/gpl-3.0.html.
+ *
+ * For further information, please contact us at sharemind@cyber.ee.
+ */
+
+#ifndef SHAREMIND_LIBAS_EXCEPTION_H
+#define SHAREMIND_LIBAS_EXCEPTION_H
+
+#include <sharemind/Exception.h>
+#include <sharemind/ExceptionMacros.h>
+
+
+namespace sharemind {
+namespace Assembler {
+
+SHAREMIND_DECLARE_EXCEPTION_CONST_STDSTRING_NOINLINE(sharemind::Exception,
+                                                     Exception);
+
+} /* namespace Assembler { */
+} /* namespace sharemind { */
+
+#endif /* SHAREMIND_LIBAS_EXCEPTION_H */
